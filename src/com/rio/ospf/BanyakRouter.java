5b3f92891261791169caa865fca24a88be1f19a3
andiyulistyo <andiyulistyo@gmail.com>
2014-01-02 23:42:26 +0700

    Initial commit

---
+++ src/com/rio/ospf/BanyakRouter.java
@@ -0,0 +1,15 @@
+package com.rio.ospf;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BanyakRouter extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
