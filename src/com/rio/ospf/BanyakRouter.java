727c2367b316473af3d63371118d51639aeca0bb
andiyulistyo <andiyulistyo@gmail.com>
2014-01-03 11:01:52 +0700

    tambah untuk input nama router

--- src/com/rio/ospf/BanyakRouter.java
+++ src/com/rio/ospf/BanyakRouter.java
@@ -1,15 +1,35 @@
 package com.rio.ospf;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class BanyakRouter extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+public class BanyakRouter extends Activity implements View.OnClickListener {
+
+    private EditText editTextInput;
+    private Button buttonNext;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        editTextInput = (EditText) findViewById(R.id.editTextBanyakRouter);
+        buttonNext = (Button) findViewById(R.id.buttonSelanjutnya);
+
+        buttonNext.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int router = Integer.valueOf(editTextInput.getText().toString());
+
+        Intent intent = new Intent(getApplication(), NamaRouter.class);
+        intent.putExtra("router", router);
+        startActivity(intent);
     }
 }
