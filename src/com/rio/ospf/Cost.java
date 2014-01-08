8b533b94331bedfcc7454dba997eaf46c0fd6939
andiyulistyo <andiyulistyo@gmail.com>
2014-01-09 01:29:25 +0700

    add layout and class

---
+++ src/com/rio/ospf/Cost.java
@@ -0,0 +1,19 @@
+package com.rio.ospf;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by souttab on 08/01/14.
+ */
+public class Cost extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_cost);
+
+    }
+
+}
---
+++ src/com/rio/ospf/Ospf.java
@@ -0,0 +1,16 @@
+package com.rio.ospf;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by souttab on 08/01/14.
+ */
+public class Ospf extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_ospf);
+    }
+}
